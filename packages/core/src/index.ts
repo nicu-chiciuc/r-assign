fb3a7194132d84fb728a15929baa93eaba2be9ea
Nicu Chiciuc <nicuchiciuc@gmail.com>
2023-03-04 15:10:50 +0200

    add some example in zod and r-assign

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -83,3 +83,5 @@ export const rAssign = <S extends TransformSchema<any>>(
 
   return result;
 };
+
+export * from './lib';
--- packages/example-cli/src/main.ts
+++ packages/example-cli/src/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import * as r from '@r-assign/core';
+import * as z from 'zod';
+
+const s: unknown = 4;
+
+const zodSchema = z.object({
+  number: z.number(),
+  string: z.string(),
+});
+
+const rAssignSchema = r.object({
+  number: r.number,
+  string: r.string,
+});
+
+if (rAssignSchema(s)) {
+  console.log(s.number);
+  console.log(s.string);
+}
